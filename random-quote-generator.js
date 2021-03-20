a82a141d368b60683029c88e5884a29f50f6fd3b
Becky Newton <iambexnewton@gmail.com>
2021-03-20 13:18:50 +0000

    sat20march

---
+++ random-quote-generator.js
@@ -0,0 +1,86 @@
+let quotes = [
+  {
+    "Quote":
+      "Too many good docs are getting out of the business. Too many OB-GYNs arent able to practice their love with women all across this country.",
+    "Author": 'President George W. Bush, Sept. 6, 2004',
+  },
+  {
+    "Quote":
+      "I'll be long gone before some smart person ever figures out what happened inside this Oval Office.",
+    "Author":
+      "President George W. Bush, in an interview with the Jerusalem Post, Washington, D.C., May 12, 2008",
+  },
+  {
+    "Quote":
+      "We know there are known knowns: there are things we know we know. We also know there are known unknowns: that is to say we know there are things we know we don't know. But there are also unknown unknowns — the ones we don't know we don't know.",
+    "Author":
+      "Defense Secretary Donald Rumsfeld, Defense Department briefing, Feb. 12, 2002",
+  },
+  {
+    "Quote": 'They misunderestimated me.',
+    "Author": 'President George W. Bush, Nov. 6, 2000',
+  },
+  {
+    "Quote": 'Our enemies are innovative and resourceful, and so are we. They never stop thinking about new ways to harm our country and our people, and neither do we.',
+    "Author": 'President George W. Bush, Aug. 5, 2004',
+  },
+  {
+    "Quote": 'Shes a wonderful, wonderful person, and were looking to a happy and wonderful night — ah, life.',
+    "Author": 'Sen. Ted Kennedy, speaking about his then-fiancee, Victoria Reggie',
+  },
+  {
+    "Quote": "Blessed are the young, for they shall inherit the national debt.",
+    "Author": "President Herbert Hoover",
+  },
+  {
+    "Quote": "Considering the dire circumstances that we have in New Orleans, virtually a city that has been destroyed, things are going relatively well.",
+    "Author": "FEMA Director Michael Brown, in the aftermath of Hurricane Katrina, Sept. 1, 2005",
+  },
+  {
+    "Quote": "The honorable Member is living proof that a pig’s bladder on a stick can be elected to Parliament." ,
+    "Author":"Labor Member of Parliament Tony Banks on Tory Member of Parliament Terry Dicks " ,
+  },
+  {
+    "Quote": "I don’t want to be rude but, really, you have the charisma of a damp rag and the appearance of a low-grade bank clerk." ,
+    "Author": "Former U.K. Independence Party leader Nigel Farage on European Council President Herman Van Rompuy." ,
+  },
+  {
+    "Quote":"The house has noticed the Prime Minister’s remarkable transformation in the last few weeks from Stalin to Mr. Bean." ,
+    "Author": "British Liberal Democrat Vincent Cable on Tory British Prime Minister Gordon Brown." ,
+  },
+  
+  {
+    "Quote": "Yes, cannabis is dangerous, but no more than other perfectly legal drugs. It’s time for a rethink, and the Tory party, the funkiest, most jiving party on Earth is where it’s happening.",
+    "Author": "Boris Johnson",
+  },
+  {
+    "Quote": "I’m as fit as a butcher’s dog now … Do you want me to do some press-ups to show you how fit I am?" ,
+    "Author": "Boris Johnson in a June interview, a couple of months after getting out of hospital – where he received intensive care for Covid.",
+  },
+  {
+    "Quote":" " ,
+    "Author":" " ,
+  },
+  {
+    "Quote": " ",
+    "Author": "" ,
+  },
+  {
+    "Quote": ,
+    "Author": ,
+  },
+  {
+    "Quote": ,
+    "Author": ,
+  },
+  {
+    "Quote": ,
+    "Author": ,
+  },
+
+];
+
+function displayQuote(){
+    return quotes
+}
+
